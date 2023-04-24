94930d3e2255cf0a7a9df0a911b7ee0267aeae71
guvalenzuelam <guvalenzuelam@gmail.com>
2023-04-24 19:05:04 -0400

    Primera Entrega

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,25 +6,32 @@ const productManager = new ProductManager("products.json");
 console.log(productManager);
 const router = Router();
 
-//para llamar metodos asincronos tengo que usar funciones asincronas
 router.get("/", async(req,res)=>{
     try {
-        const products =await productManager.getProduct();
-        res.json({status:"success", data:products});   
+        const products = await productManager.getProduct();
+        const limit = parseInt(req.query.limit); // Accede al valor del query param limit y lo convierte a un número entero
+        if (isNaN(limit)) { // Si el valor de limit no es un número, devuelve todos los productos
+            res.json({status: "success", data: products});
+        } else if (limit <= 0) { // Si el valor de limit es menor o igual a cero, devuelve un error
+            res.status(400).json({status: "error", message: "Error de tipo 1"});
+        } else { // Si el valor de limit es un número válido, devuelve una cantidad limitada de productos
+            const limitedProducts = products.slice(0, limit);
+            res.json({status: "success", data: limitedProducts});
+        }
     } catch (error) {
-        res.status(400).json({status:"error",message:error.message});
+        res.status(400).json({status: "error", message: error.message});
     }
 });
 
 router.get("/:pid", async(req,res)=>{
-        const productId = req.params.pid;
-        const products =await productManager.getProduct();
-        const product = products.find(item=>item.id ===parseInt(productId));
-        if(product){
-        res.json({status:"success", data:product});
-        } else {
-        res.status(400).json({status:"error", message:"No existe el producto"});
-    }
+    const productId = req.params.pid;
+    const products =await productManager.getProduct();
+    const product = products.find(item=>item.id ===parseInt(productId));
+    if(product){
+    res.json({status:"success", data:product});
+    } else {
+    res.status(400).json({status:"error", message:"No existe el producto"});
+}
 });
 
 router.post("/", async(req,res)=>{
