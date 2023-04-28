bbd512e27af5bf58ec4032f4fbf5e637e3b90743
guvalenzuelam <guvalenzuelam@gmail.com>
2023-04-27 21:38:31 -0400

    Primera Entrega

--- src/app.js
+++ src/app.js
@@ -9,29 +9,6 @@ const port = 8080;
 
 app.listen(port,()=>console.log(`Server listening on port${port}`));
 
-/*
-app.get("/",(request,response)=>{
-    response.send("Bienvenido al Desafio 3");
-});
-
-app.get('/products', (req, res) => {
-    const limit = req.query.limit;
-    
-    fs.readFile('products.json', (err, data) => {
-      if (err) throw err;
-  
-      const products = JSON.parse(data);
-  
-      if (limit && !isNaN(limit)) {
-        res.send(products.slice(0, limit));
-      } else {
-        res.send(products);
-      }
-    });
-});
-*/
-
-
 //midlewares: Para recibir la inforamción de la petición de tipo post
 app.use(express.json());
 
