74d7954a815c395c8e657f73c1af0548f4c18b16
guvalenzuelam <guvalenzuelam@gmail.com>
2023-05-30 00:26:30 -0400

    Entrega2

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -139,10 +139,6 @@ router.delete('/api/carts/:cid', async (req, res) => {
     }
   });
   
-  
-
-
-
 //NO ENTENDI LA INSTRUCCIÓN:
 //Esta vez, para el modelo de Carts, en su propiedad products, el id de cada producto generado dentro del array tiene que hacer referencia al modelo de Products. Modificar la ruta /:cid para que al traer todos los productos, los traiga completos mediante un “populate”. De esta manera almacenamos sólo el Id, pero al solicitarlo podemos desglosar los productos asociados.
 
